4c34edc6565b3d16ca90831ca4c745919e3f80db
Carlos Pons <karlospn@gmail.com>
2017-10-12 19:59:01 +0200

    Migrate to sql

--- SocialNetwork/SocialNetwork.OAuth/Startup.cs
+++ SocialNetwork/SocialNetwork.OAuth/Startup.cs
@@ -1,8 +1,17 @@
-﻿using Microsoft.AspNetCore.Builder;
+﻿using System;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using System.Security.Cryptography.X509Certificates;
+using Microsoft.Extensions.Configuration;
+using Microsoft.EntityFrameworkCore;
+using System.Reflection;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using IdentityServer4.EntityFramework.DbContexts;
+using IdentityServer4.EntityFramework.Mappers;
 using SocialNetwork.OAuth.Configuration;
 
 namespace SocialNetwork.OAuth
@@ -21,20 +30,26 @@ namespace SocialNetwork.OAuth
 
         public IConfigurationRoot Configuration { get; }
 
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddSingleton<Func<IDbConnection>>(() => new SqlConnection(Configuration.GetConnectionString("SocialNetwork")));
+
+            var assembly = typeof(Startup).GetTypeInfo().Assembly.GetName().Name;
             services.AddIdentityServer()
                 .AddTemporarySigningCredential()
-                .AddInMemoryClients(Clients.All())
-                .AddInMemoryApiResources(ApiResources.All())
-                .AddInMemoryIdentityResources(IdentityResources.All())
-                .AddTestUsers(Users.All());
+                .AddTestUsers(Users.All())
+                .AddConfigurationStore(builder => builder.UseSqlServer(Configuration.GetConnectionString("SocialNetwork.OAuth"), options => options.MigrationsAssembly(assembly)))
+                .AddOperationalStore(builder => builder.UseSqlServer(Configuration.GetConnectionString("SocialNetwork.OAuth"), options => options.MigrationsAssembly(assembly)));
+
 
             services.AddMvc();
         }
-
         public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
         {
+            InitializeDatabase(app);
+
             loggerFactory.AddConsole(LogLevel.Debug);
             app.UseDeveloperExceptionPage();
 
@@ -42,6 +57,44 @@ namespace SocialNetwork.OAuth
 
             app.UseStaticFiles();
             app.UseMvcWithDefaultRoute();
+        }
+
+        private void InitializeDatabase(IApplicationBuilder app)
+        {
+            using (var serviceScope = app.ApplicationServices.GetService<IServiceScopeFactory>().CreateScope())
+            {
+                serviceScope.ServiceProvider.GetRequiredService<PersistedGrantDbContext>().Database.Migrate();
+
+                var context = serviceScope.ServiceProvider.GetRequiredService<ConfigurationDbContext>();
+                context.Database.Migrate();
+
+                if (!context.Clients.Any())
+                {
+                    foreach (var client in Clients.All())
+                    {
+                        context.Clients.Add(client.ToEntity());
+                    }
+                    context.SaveChanges();
+                }
+
+                if (!context.IdentityResources.Any())
+                {
+                    foreach (var resource in IdentityResources.All())
+                    {
+                        context.IdentityResources.Add(resource.ToEntity());
+                    }
+                    context.SaveChanges();
+                }
+
+                if (!context.ApiResources.Any())
+                {
+                    foreach (var resource in ApiResources.All())
+                    {
+                        context.ApiResources.Add(resource.ToEntity());
+                    }
+                    context.SaveChanges();
+                }
+            }
         }
     }
 }
