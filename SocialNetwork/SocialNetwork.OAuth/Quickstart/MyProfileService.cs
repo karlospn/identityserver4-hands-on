a628d042ad39ef2ac21f045131dd7f85ab803948
Carlos Pons <karlospn@gmail.com>
2017-11-14 00:02:55 +0100

    Add new profileservice

---
+++ SocialNetwork/SocialNetwork.OAuth/Quickstart/MyProfileService.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using IdentityServer4.Models;
+using IdentityServer4.Services;
+
+namespace SocialNetwork.OAuth.Quickstart
+{
+    public class MyProfileService : IProfileService
+    {
+        public Task GetProfileDataAsync(ProfileDataRequestContext context)
+        {
+            var claims = new List<Claim>();
+            claims.Add(new Claim("FullName", "Joe"));
+            claims.Add(new Claim("FullName", "Tester"));
+            context.IssuedClaims = claims;
+            return Task.FromResult(0);
+        }
+
+        public Task IsActiveAsync(IsActiveContext context)
+        {
+            context.IsActive = true;
+            return Task.FromResult(0);
+        }
+    }
+}
